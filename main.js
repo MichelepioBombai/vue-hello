9b356c4809462dfa44554bada1646200f94c4140
MichelepioBombai <michelebombai@icloud.com>
2023-02-09 16:42:35 +0100

    final-commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Vue ok", Vue);
+
+const app = Vue.createApp({
+    data() {
+        return{
+            message: "TITOLO PRINCIPALE"  
+        }
+
+        
+
+        
+    }
+});
+
+
+
+
+
+app.mount("#root");
